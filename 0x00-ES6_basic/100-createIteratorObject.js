ce84c00d622d651746018f4874f115237951b307
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-03 10:19:49 +0000

    fixed spaces and added comments

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,11 @@
 export default function createIteratorObject(report) {
-    const employeeInfo = report.allEmployees;
-    let iterator = []
-    for (let employee of [...Object.values(employeeInfo)]) {
-	iterator = [...iterator, ...employee];
-    }
-    return iterator;
+  /* report is an object with an object property of department names(strings) */
+  /* and an array of employees in the department called allEmployees */
+  /* and a propety method to get all the departments called getNumberOfDepartments */
+  const employeeInfo = report.allEmployees;
+  let iterator = [];
+  for (const employee of [...Object.values(employeeInfo)]) {
+    iterator = [...iterator, ...employee];
+  }
+  return iterator;
 }
