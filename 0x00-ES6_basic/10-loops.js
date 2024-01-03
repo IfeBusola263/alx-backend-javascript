ead50065dc04e07425de58762c4b2ddb0d41e74e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-03 09:20:34 +0000

    fixed with ESLint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   for (let idx of array) {
     idx = array.indexOf(idx);
     const value = array[idx];
