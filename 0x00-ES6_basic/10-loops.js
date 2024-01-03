500a2502f90da68f540ac071d8ae93fb37fb76af
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-03 10:33:34 +0000

    Fixed the no-param-reassign, with the inline ESlint configuration to set the severity level of the rule to error just for that line of code that follows it

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,6 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   for (let idx of array) {
     idx = array.indexOf(idx);
     const value = array[idx];
+    /* eslint no-param-reassign: "error" */
     array[idx] = appendString + value;
   }
 
