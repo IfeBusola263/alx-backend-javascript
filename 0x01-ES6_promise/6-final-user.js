b3edaa6eccdd997f5779913326049cc37abda2aa
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-03 23:23:48 +0000

    returned the value of the promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const settled = Promise.all([userPromise, photoPromise]).catch(() => {});
+  return settled;
+}
