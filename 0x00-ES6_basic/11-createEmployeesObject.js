6823e239bd879a66a60cb7d0a810dbbe84edad8a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-03 10:09:46 +0000

    Fixed dangling comma on object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
+  const departmentEmployee = {
+    [`${departmentName}`]: employees,
+  };
 
-    const departmentEmployee = {
-	[`${departmentName}`]: employees,
-    };
-
-    return departmentEmployee;
+  return departmentEmployee;
 }
