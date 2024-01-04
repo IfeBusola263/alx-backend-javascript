491e57968243676e3f9df67076742a1d65525b7f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-04 01:20:36 +0000

    Changed the syntax

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((success) => success);
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+	.then((success) => success);
 }
