31f165a4beee6adff46fad6661cfc5b1ce6a6f1b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-04 01:05:48 +0000

    Fixed the syntax

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ const photoPromise = uploadPhoto();
 const userPromise = createUser();
 
 export default function handleProfileSignup() {
-  return Promise.all([photoPromise, userPromise]).then((Value) => {
-    console.log(`${Value[0].body} ${Value[1].firstName} ${Value[1].lastName}`);
+  return Promise.all([photoPromise, userPromise]).then(([photoRes, userRes]) => {
+    console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
   }).catch(() => { console.error('Signup system offline'); });
 }
