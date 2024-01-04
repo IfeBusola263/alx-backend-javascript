3d512095cd09ddc35c06005787f971e1dc52579e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-04 00:55:31 +0000

    Outputed the fulfilment Value

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,9 +4,7 @@ const photoPromise = uploadPhoto();
 const userPromise = createUser();
 
 export default function handleProfileSignup() {
-  return Promise.all([photoPromise, userPromise]).then((success) => {
-    console.log(`${success.body} ${success.firstName} ${success.lastName}`);
-  }).catch(() => {
-    console.error('Signup system offline');
-  });
+  return Promise.all([photoPromise, userPromise]).then((Value) => {
+    console.log(`${Value[0].body} ${Value[1].firstName} ${Value[1].lastName}`);
+  }).catch(() => { console.error('Signup system offline'); });
 }
