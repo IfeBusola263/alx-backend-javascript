b196a2d50cddafad79f82f308decdf648ac24e73
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-04 01:17:08 +0000

    Fixed the promise method

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-    return Promise.all([userPromise, photoPromise]).then((success) => {
-	return [ { status: success.status,
-		   value: success.status === 'fulfilled' ? success.value : success.reason,
-		 }
-	       ]
-    }).catch(() => {});
+  return Promise.allSettled([userPromise, photoPromise]).then((success) => success);
 }
