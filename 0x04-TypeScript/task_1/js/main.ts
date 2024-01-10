782ef4163c30837e54bac726d4775c2a480ab177
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-10 21:57:13 +0100

    Implemented a student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,37 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;   
 }
+
+function printTeacher(firstName: string, lastName: string): string {
+    const firstNameLetter: string = firstName.slice(0);
+    const name: string = firstNameLetter + '.' + ' ' + lastName;
+    return name;
+}
+
+interface studentClassConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentInter {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInter {
+    firstName: string;
+    lastName: string;
+
+    constructor({firstName, lastName}: studentClassConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
