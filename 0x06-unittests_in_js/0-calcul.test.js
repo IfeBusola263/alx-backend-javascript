1b8a5b0153beb8c4901a0876be25e2dbe90250ac
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-29 19:14:00 +0000

    Another trial

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
+    it('should round 2.0 and 4.9 to give -7', function () {
+	assert.strictEqual(calculateNumber(-2.0, -4.9), -7);
+    });
+
+    it('should round -3.5 and -7.2 to give -10', function () {
+	assert.strictEqual(calculateNumber(-3.5, -7.2), -10);
+    });
+
     it('should round 3 and 4 to give 1', function () {
 	assert.strictEqual(calculateNumber(-3, 4), 1);
     });
 
     it('should round 3.5 and 4.2 to give 8', function () {
 	assert.strictEqual(calculateNumber(3.5, 4.2), 8);
-    });
-    it('should round 2.0 and 4.9 to give 7', function () {
-	assert.strictEqual(calculateNumber(2.0, 4.9), 7);
-    });
-
-    it('should round -3.5 and -7.2 to give -10', function () {
-	assert.strictEqual(calculateNumber(-3.5, -7.2), -10);
     });
 
     it('should round 0 and -20.9 to give -21', function () {
