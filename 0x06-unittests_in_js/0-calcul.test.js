171a6aeea92b6cf1363ebc3ebba7e56361a0bb97
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-29 18:15:32 +0000

    Test for calculat numbers

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+console.log(calculateNumber);
+
+describe('calculateNumber', function () {
+    it('should return the addition of the rounded values of the args', function () {
+	assert.strictEqual(calculateNumber(3, 4), 7);
+	assert.strictEqual(calculateNumber(3.5, 4.2), 9);
+	assert.strictEqual(calculateNumber(2.0, 4.9), 7);
+	assert.strictEqual(calculateNumber(-3.5, 7.2), 5);
+	assert.strictEqual(calculateNumber(0, -20.9), -20);
+    });
+});
+	
