1fc93c0b3bf0f51ff2c52de7d88a6c3734fd7c43
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-01 12:24:52 +0000

    mend

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function () {
+describe('SUM', function () {
   it('Should add a and b if type is SUM', function () {
     expect(calculateNumber('SUM', -1.2, -3.3)).to.equal(-4);
     expect(calculateNumber('SUM', -5.8, -3.3)).to.equal(-9);
@@ -12,7 +12,9 @@ describe('calculateNumber', function () {
     expect(calculateNumber('SUM', 11.2, 30.3)).to.equal(41);
     expect(calculateNumber('SUM', 11.2)).to.be.NaN;
   });
+});
 
+describe('SUBTRACT', function () {
   it('Should Subtract b from a if type  is SUBTRACT', function () {
     expect(calculateNumber('SUBTRACT', -1.2, -3.3)).to.equal(2);
     expect(calculateNumber('SUBTRACT', -5.8, -3.3)).to.equal(-3);
@@ -20,7 +22,9 @@ describe('calculateNumber', function () {
     expect(calculateNumber('SUBTRACT', 5.8, -3.3)).to.equal(9);
     expect(calculateNumber('SUBTRACT', 5.8)).to.be.NaN;
   });
+});
 
+describe('DIVIDE', function () {
   it('Should divide a by b if type is DIVIDE', function () {
     expect(calculateNumber('DIVIDE', -1.2, -2.3)).to.equal(0.5);
     expect(calculateNumber('DIVIDE', -5.8, -3.3)).to.equal(2);
@@ -28,7 +32,9 @@ describe('calculateNumber', function () {
     expect(calculateNumber('DIVIDE', 15.2, 2.7)).to.equal(5);
     expect(calculateNumber('DIVIDE', -3.3)).to.be.NaN;
   });
+});
 
+describe('ERROR', function () {
   it("Should return 'Error' if type is DIVIDE and b rounds to 0", function () {
     expect(calculateNumber('DIVIDE', -1.2, -0.5)).to.equal('Error');
     expect(calculateNumber('DIVIDE', -5.8, -0.3)).to.equal('Error');
