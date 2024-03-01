b8edcb4d2c234fcb6ae0abe4a61e8b5d2a99cb9a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-01 12:17:56 +0000

    fixed the variable definition in the file

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -5,8 +5,8 @@ function calculateNumber(type, a, b) {
     else if  (type === 'SUBTRACT')
 	return Math.round(a) - Math.round(b);
     else if  (type === 'DIVIDE') {
-	num = Math.round(a)
-	denom = Math.round(b);
+	const num = Math.round(a)
+	const denom = Math.round(b);
 	if (denom !== 0)
 	    return num / denom;
 	return 'Error';
